822d0bdac78df1344f39a1df293e7bd00cd4d695
chanduaadi <chanduaadi2002@gmail.com>
2023-01-19 14:15:55 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <img
+        className="website-logo"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+        alt="website logo"
+      />
+      <ul className="nav-menu">
+        <li className="nav-link">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-link">Products</li>
+        <li className="nav-link">Cart</li>
+      </ul>
+      <button type="button" className="logout-desktop-btn">
+        Logout
+      </button>
+      <button type="button" className="logout-mobile-btn">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+          alt="logout icon"
+          className="logout-icon"
+        />
+      </button>
+    </div>
+  </nav>
+)
+export default Header
